c1d1fcbe5c1ee6948b5a8b13832ec0458dad065d
JCPedroza <nemesis4go10@hotmail.com>
2022-02-04 07:13:05 -0600

    test: stop using the mock db and define data directly

--- src/components/layout/item/Item.test.js
+++ src/components/layout/item/Item.test.js
@@ -2,21 +2,13 @@ import { render } from '@testing-library/react'
 
 import Item from './Item'
 
-import db from '../../../db'
-
 const itemData = { id: 1, title: 'One', author: 'Mario', year: 1926 }
 
-jest.mock('../../../db', () => ({
-  ...jest.requireActual('../../../db'),
-  getById: jest.fn().mockReturnValue(itemData)
-}))
-
-const song = db.getById(1)
 const keysToShow = ['title', 'year']
 
 it('renders item', () => {
   const { container } = render(
-    <Item item={song} keysToShow={keysToShow} />
+    <Item item={itemData} keysToShow={keysToShow} />
   )
 
   expect(container).toMatchSnapshot()
