b7bf1b0b11eb75f14e351f92c8d0bd7abc7f4295
JCPedroza <nemesis4go10@hotmail.com>
2022-02-25 01:22:44 -0600

    feat: add coming soon message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-import Item from './components/layout/item/Item'
-import List from './components/layout/list/List'
-
-import Song from './db'
-
-const App = () => {
-  return (
-    <List
-      items={Song.find()}
-      keysToShow={['id', 'title', 'author', 'year']}
-      resourceName='item'
-      itemComponent={Item}
-    />
-  )
-}
+const App = () => <h1>Coming soon!</h1>
 
 export default App
